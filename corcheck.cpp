4963147d64dbce4de7190949be254c98592eb73d
relax212 <kirik3132@gmail.com>
2023-06-05 01:58:37 +0300

    tetris

---
+++ corcheck.cpp
@@ -0,0 +1,135 @@
+#include "tetr.h"
+#include "figures.h"
+
+int func_random(){
+    srand(time(NULL));
+    int num = rand() % 7;
+    return num;
+}
+
+
+void copy(int pole[ROWS][COLUMNS],int pole2 [ROWS][COLUMNS]){
+    for (int p = 0; p < ROWS; ++p){
+        for (int o = 0; o < COLUMNS; ++o){
+            pole [p][o] = pole2 [p][o];
+        }
+    }
+}
+
+
+bool corrector(int pole2 [ROWS][COLUMNS], int k, int  h, int z,int a){
+    int pole3 [ROWS][COLUMNS];
+    copy(pole3, pole2);
+    for (int i = 0; i < 4; ++i ){
+        for (int j = 0; j < 4; ++j){
+            pole3[i+k][j+5+h] += tetrominoes[a][z][i][j];
+        }
+    }
+    for (int i = 0; i < ROWS; ++i ){
+        for (int j = 0; j < COLUMNS; ++j){
+            if (pole3[i][j] > 9) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void correct(int pole[ROWS][COLUMNS],int pole2 [ROWS][COLUMNS], int k, int  h, int &x, int z,int a){
+    copy(pole,pole2);
+    int pole3 [ROWS][COLUMNS];
+    copy(pole3,pole2);
+    for (int i = 0; i < 4; ++i ){
+        for (int j = 0; j < 4; ++j){
+            pole3[i+k][j+5+h] += tetrominoes[a][z][i][j];
+        }
+    }
+    for (int i = 0; i < ROWS; ++i ){
+        for (int j = 0; j < COLUMNS; ++j){
+            if (pole3[i][j] > 9) {
+                k--;
+                for (int i = 0; i < 4; ++i ){
+                    for (int j = 0; j < 4; ++j){
+                        pole[i+k][j+5+h] += tetrominoes[a][z][i][j];
+                    }
+                }
+                for (int i = 0; i < ROWS-1; ++i ){
+                    for (int j = 0; j < COLUMNS; ++j){
+                        if (pole[i][j] != 0){
+                            pole[i][j] = 9;
+                        }
+                    }
+                }
+                x = 1;
+                return;
+            }
+        }
+    }
+    copy(pole,pole3);
+}
+
+
+
+void turns(int pole2[ROWS][COLUMNS], int &h, int &z, char x, int &k, int a){
+    switch (x) {
+        case 'D':
+              h--;
+            if (!corrector(pole2,k,h,z,a))
+                h++;
+            break;
+        case 'C':
+                h++;
+            if (!corrector(pole2,k,h,z,a))
+                h--;
+            break;
+        case 'A':
+            z++;
+            if (z == 4)
+                z = 0;
+            if (!corrector(pole2,k,h,z,a)){
+                z--;
+                if (z == -1)
+                    z = 3;
+            }
+            break;
+        case 'B':
+            if (k < ROWS-5){
+            k++;
+            }
+            if (!corrector(pole2,k,h,z,a)){
+                k--;
+            }
+            break;
+    }
+}
+
+
+
+
+void check (int pole[ROWS][COLUMNS], int &score){
+     for (int i = 0; i < ROWS - 1; ++i ){
+        int flag = 0;
+        for (int j = 0; j < COLUMNS; ++j){
+            if (pole[i][j] != 9){
+                flag = 1;
+            }
+        }
+        if (flag == 0){
+            for (int x = i; x > 1; --x){
+                for (int j = 1; j < COLUMNS - 1; ++j){
+                pole [x][j] = 0;
+                pole [x][j] += pole[x-1][j];
+                }
+            }
+            score += 5000;
+        }
+     }
+}
+bool results (int pole2[ROWS][COLUMNS]){
+    for (int j = 1; j < COLUMNS - 1; ++j){
+        if(pole2[3][j] == 9){
+            return true;
+        }
+    }
+    return false;
+ }
---
+++ tetr.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <unistd.h>
+#include <termios.h>
+#include <fcntl.h>
+
+#define ROWS 26
+#define COLUMNS 14
+#define RED "\e[41m"
+#define CLEAN "\e[0m"
+#define GREEN "\e[42m"
+#define BLUE "\e[44m"
+#define WHITE "\e[47m"
+#define FOREGROUND_BLACK "\e[1;30m"
+#define FOREGROUND_BLUE "\e[0;34m"
+#define FOREGROUND_RED "\e[0;31m"
+
+void output(int pole[ROWS][COLUMNS],int,int);
+void fill (int pole[ROWS][COLUMNS]);
+void set_keypress(int pole2[ROWS][COLUMNS],int&,int&,int&,int);
+int func_random();
+void copy(int pole[ROWS][COLUMNS],int pole2[ROWS][COLUMNS]);
+bool corrector(int pole2[ROWS][COLUMNS],int,int,int,int);
+void correct(int pole[ROWS][COLUMNS],int pole2[ROWS][COLUMNS],int,int,int&,int,int);
+void turns(int pole2[ROWS][COLUMNS],int&,int&,char,int&,int);
+void check(int pole[ROWS][COLUMNS],int&);
+bool results(int pole2[ROWS][COLUMNS]);
