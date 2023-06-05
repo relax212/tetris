a93add8b9766e361243b39d5245752d67c6f5e1c
relax212 <kirik3132@gmail.com>
2023-06-05 14:54:56 +0300

    tetris

--- output.cpp
+++ output.cpp
@@ -1,5 +1,5 @@
 #include "tetr.h"
-#include "figures.h"
+extern int tetrominoes[7][4][5][4];
 
 static struct termios stored_settings;
 
@@ -60,7 +60,7 @@ void set_keypress(int pole2[ROWS][COLUMNS], int &h, int &z, int &k, int a)
   char c;
   tcgetattr(STDIN_FILENO, &oldt);
   newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
+  newt.c_lflag &= ~(ICANON);
   tcsetattr(STDIN_FILENO, TCSANOW, &newt);
   oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
   fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
